b7a365ce56ba296cf8c2eb0bda642e16aae62448
Sakshi12-web <sgour5017@gmail.com>
2026-09-09 12:38:15 +0000

    area of circle

---
+++ basic/AreaOfCircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float radius;
+    cout<<"Enter radius : ";
+    cin>>radius;
+    float pi = 3.14;
+    float area = pi*radius*radius;
+    cout<<area;
+    return 0;
+}
