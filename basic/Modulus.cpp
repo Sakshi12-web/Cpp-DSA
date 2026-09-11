09b0ce9702b8f90184b3ac492cecb7e710fef7ab
Sakshi12-web <sgour5017@gmail.com>
2026-09-11 14:18:17 +0000

    Modulus Operator

---
+++ basic/Modulus.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter your first number:";
+    cin>>a;
+    int b;
+    cout<<"Enter your second number:";
+    cin>>b;
+    cout<<"The reminder of "<<a<<" % "<<b<<" is: "<<a%b<<endl;
+    return 0;
+}
