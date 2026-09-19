0d0959cd2a2fa9003bd4ead2b86a45c2b1f9afe8
Sakshi12-web <sgour5017@gmail.com>
2026-09-19 05:54:06 +0000

    Division of two numbers

---
+++ basic/Division.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter your first number : ";
+    cin>>a;
+    cout<<"Enter your second number : ";
+    cin>>b;
+    int division = a/b;
+    cout<<a<<" divided by "<<b<<" is "<<division;
+    return 0;
+}
