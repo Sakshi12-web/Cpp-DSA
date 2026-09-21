d59bf44611b62f79f3d2cab7a8912575730206de
Sakshi12-web <sgour5017@gmail.com>
2026-09-21 11:58:09 +0000

    sum of two interger

---
+++ basic/sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Entr first number : ";
+    cin>>a;
+    cout<<"Enter second number : ";
+    cin>>b;
+    cout<<"The sum of "<<a<<" + "<<b<<" is = "<<a+b;
+    return 0;
+}
